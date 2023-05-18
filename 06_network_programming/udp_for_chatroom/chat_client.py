2b16516a1520b02961b358a4bbbdf806117ddfe8
zhl <zhl888@petalmail.com>
2023-05-18 14:31:17 +0800

    day01

--- 06_network_programming/udp_for_chatroom/chat_client.py
+++ 06_network_programming/udp_for_chatroom/chat_client.py
@@ -1,31 +1,46 @@
 import socket, os
 
+def login(s):
+    while True:
+        name = input("Please input your name:")
+        s.sendto(("L %s "%name).encode(), ADDR)
+        msg,addr = s.recvfrom(128)
+        if msg.decode() == "OK":
+            print("你进入了聊天室")
+            break
+        elif msg.decode() == "name exist":
+            print("Name exist! Please input again")
+            continue
+    return name
+
+def chat(s, name):
+    while True:
+        msg = input("msg>>")
+        if msg.strip() == "Q":
+            s.sendto(("Q %s"%name).encode(), ADDR)
+            print("退出聊天室")
+            os._exit(1)
+        else:
+            s.sendto(("M %s %s"%(name, msg)).encode(), ADDR)
+            
+def recv(s):
+    while True:
+        msg, addr = s.recvfrom(1024)
+        if msg.decode() == "EXIT":
+            os._exit(1)
+        print(msg.decode())
+        print("msg>>", end = "")
+
 ADDR = ("127.0.0.1",8888)
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-while True:
-    name = input("Please input your name:")
-    s.sendto(("L %s"%name).encode(), ADDR)
-    msg,addr = s.recvfrom(128)
-    if msg.decode() == "OK":
-        print("你进入了聊天室")
-        break
-    elif msg.decode() == "name exist":
-        print("Name exist! Please input again")
-        continue
-while True:
-    msg = input("msg>>>")
-    if msg.strip == "Q":
-        s.sendto(("Q"%msg).encode(), ADDR)
-        os._exit()
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    name = login(s)
+    pid = os.fork()
+    if pid == 0:
+        recv(s)
     else:
-        s.sendto(("M %s"%msg).encode(), ADDR)
-    
+        chat(s, name)
 
-
-# while True:
-#     s.sendto("data",ADDR)
-
-#     msg,addr = s.recvfrom(2048)
-# s.close
+if __name__ == "__main__":
+    main()
+ 
--- 06_network_programming/udp_for_chatroom/chat_server.py
+++ 06_network_programming/udp_for_chatroom/chat_server.py
@@ -12,33 +12,63 @@
     "M":发言
     "Q":退出
 """
-
 import socket, os, signal
-
 signal.signal(signal.SIGCHLD, signal.SIG_IGN) #处理子进程退出,防止产生僵尸进程
-
 ADDR = ("127.0.0.1",8888)
 userdict = {}
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(ADDR)
-print("waiting for connect...")
-# pid = os.fork()
-# if pid == 0:
-while True:
-    data,addr = s.recvfrom(2048)
-    mark = data.decode().split(" ")[0]
-    content = " ".join(data.decode().split(" ")[1:]).strip()
-    if mark == "L":
-        name = content
-        if name not in userdict:
-            s.sendto(b"OK", addr)
-            userdict[name] = addr
-        else:
-            s.sendto(b"name exist",addr)
-    elif mark == "M": 
-        for name,addr in userdict.items():
-            s.sendto(content.encode(),addr)
 
-# else:
-#     s.sendto(b"123")
-s.close()
+
+def login(s, name, addr):
+    if name not in userdict:
+        s.sendto(b"OK", addr)
+        msg = ("\n%s进入了聊天室"%name).encode()
+        chat(s, name, msg)
+        userdict[name] = addr
+        print(userdict)
+    else:
+        s.sendto(b"name exist",addr)
+
+def chat(s, name, msg):
+    for p, addr in userdict.items():
+        if p != name:
+            s.sendto(msg, addr)
+
+def quit(s, name, addr):
+    s.sendto(b"EXIT", addr)
+    del userdict[name]
+   
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(ADDR)
+    print("waiting for connect...")
+    pid = os.fork()
+    if pid == 0:
+        while True:
+            data,addr = s.recvfrom(2048)
+            mark = data.decode().split(" ")[0]
+            name = data.decode().split(" ")[1]
+            content = " ".join(data.decode().split(" ")[2:]).strip()
+            print(mark, name, content)
+
+            if mark == "L":
+                login(s, name, addr)
+
+            elif mark == "M": 
+                content = ("\n%s说:"%name + content).encode()
+                chat(s, name, content)
+
+            elif mark == "Q":
+                quit(s, name, addr)
+                msg = ("\n%s退出了聊天室"%name).encode()
+                chat(s, name, msg)
+    else:
+        while True:
+            msg = input("管理员消息:")
+            msg = ("M 系统管理员 %s"%msg).encode()
+            s.sendto(msg, ADDR)
+    s.close()
+
+
+if __name__ == "__main__":
+    main()
